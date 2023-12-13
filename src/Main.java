b8c36dc13efcf0f53f56729e57413897daeec17b
ehsansdr <sehsan251@gmail.com>
2023-12-13 10:39:04 +0330

    Revert "after does"
    This reverts commit 90d9976e2d4cba104f94059161a2358f494782e1.

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@ public class Main {
     public static void main(String[] args) {
         //you can use defualt constructure or difine ingoes such as name and password by hash map
         //so create  your hash map to compare the value
-
         new LogFrame();
-
         //or
         //new Logframe(new hashmap);
         //and declare your own hash map concludes with name and password...
         
-
+        //kkkk
     }
 }
