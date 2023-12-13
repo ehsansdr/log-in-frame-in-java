93226e3bd53ffab229bd104c298c58d5327c35df
ehsansdr <sehsan251@gmail.com>
2023-12-13 09:23:52 +0330

    upload 4 commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         //you can use defualt constructure or difine ingoes such as name and password by hash map
         //so create  your hash map to compare the value
+        //2314
         new LogFrame();
     }
 }
